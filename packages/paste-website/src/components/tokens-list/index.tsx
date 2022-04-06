0c159ab61d7c3a0c53afb848a9da61dc3f013ad9
Nora Krantz <nkrantz@twilio.com>
2022-04-06 19:48:10 -0400

    chore(website): pull out helpers and types from tokens-filter

--- packages/paste-website/src/components/tokens-list/index.tsx
+++ packages/paste-website/src/components/tokens-list/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {trackCustomEvent} from 'gatsby-plugin-google-analytics';
 import {Box} from '@twilio-paste/box';
 import {Stack} from '@twilio-paste/stack';
 import {Label} from '@twilio-paste/label';
@@ -10,17 +9,15 @@ import {Input} from '@twilio-paste/input';
 import {Table, Tr, Th, Td, THead, TBody} from '@twilio-paste/table';
 import {Text} from '@twilio-paste/text';
 import {useUID} from '@twilio-paste/uid-library';
-import type {ThemeVariants} from '@twilio-paste/theme';
 import {navigate} from '@reach/router';
 import {InlineCode} from '../Typography';
 import {AnchoredHeading} from '../Heading';
-import {Callout, CalloutTitle, CalloutText} from '../callout';
 import {TokenExample} from './TokensExample';
 import {getTokenValue} from './getTokenValue';
 import {useDarkModeContext} from '../../context/DarkModeContext';
 import {useLocationPathname, useLocationSearch} from '../../utils/RouteUtils';
-
-const debounce = require('lodash/debounce');
+import {trackTokenFilterString, filterTokenList, getTokensByTheme} from './helpers';
+import type {Token, TokenCategory, TokensListProps} from './types';
 
 const sentenceCase = (catName: string): string => {
   return catName
@@ -31,117 +28,20 @@ const sentenceCase = (catName: string): string => {
     });
 };
 
-interface Token {
-  name: string;
-  value: string;
-  comment: string;
-  category: string;
-  type: string;
-  deprecated: boolean;
-}
-
-interface TokenCategory {
-  categoryName: string;
-  info?: React.ReactNode;
-  tokens: Token[];
-}
-
-interface TokensShape {
-  node: {
-    tokens: TokenCategory[];
-  };
-}
-
-interface TokensListProps {
-  children?: React.ReactElement;
-  defaultTokens: TokensShape[];
-  darkTokens: TokensShape[];
-}
-
-const filterDeprecatedTokens = (tokens: TokenCategory[]): TokenCategory[] => {
-  return tokens.map((category) => {
-    return {...category, tokens: [...category.tokens.filter((token) => !token.deprecated)]};
-  });
-};
-
-const getTokensByTheme = (props: TokensListProps, theme: ThemeVariants): TokenCategory[] => {
-  const unfilteredTokensBasedOnTheme =
-    theme === 'default' ? props.defaultTokens[0].node.tokens : props.darkTokens[0].node.tokens;
-  let tokens = [] as TokenCategory[];
-
-  if (unfilteredTokensBasedOnTheme != null) {
-    tokens = filterDeprecatedTokens(unfilteredTokensBasedOnTheme);
-  }
-
-  const fontSize = tokens.find((ele) => ele.categoryName === 'font-sizes');
-  if (fontSize) {
-    fontSize.info = (
-      <Callout>
-        <CalloutTitle as="h4">Heads up about font sizes in Paste!</CalloutTitle>
-        <CalloutText>
-          Font-sizes in our Paste tokens use rem values. Unlike em values which are relative to their parent element,
-          rem values are relative to the html element. If you aren&apos;t using the Theme.Provider component, you must
-          set font-size: 100%; on your page&apos;s html tag for the font-sizes to be sized correctly as 1rem=16px.
-        </CalloutText>
-      </Callout>
-    );
-  }
-
-  return tokens;
-};
-
 const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
-
-const trackTokenFilterString = debounce((filter: string): void => {
-  if (filter !== '') {
-    trackCustomEvent({
-      category: 'Design Tokens',
-      action: 'filter',
-      label: filter,
-    });
-  }
-}, 500);
-
-const filterTokenList = (
-  filter: string,
-  // callback: React.Dispatch<React.SetStateAction<TokenCategory[] | null>>,
-  propsArg: TokensListProps,
-  themeArg: string
-): TokenCategory[] | null => {
-  // callback(() => {
-  const newTokenCategories = getTokensByTheme(propsArg, themeArg).map(
-    (category): TokenCategory => {
-      const newTokens = category.tokens.filter((token) => {
-        return token.name.includes(filter) || token.value.includes(filter);
-      });
-      return {...category, tokens: newTokens};
-    }
-  );
-  const filteredCategories = newTokenCategories.filter((category) => {
-    return category.tokens.length > 0;
-  });
-  if (filteredCategories.length > 0) {
-    return filteredCategories;
-  }
-  return null;
-  // }
-  // );
-};
 
 export const TokensList: React.FC<TokensListProps> = (props) => {
   const pathname = useLocationPathname();
   const search = useLocationSearch();
   const {theme} = useDarkModeContext();
+  // Find an existing search param in the URL, otherwise use an empty string
   const initialFilterString = search ? search.replace('?tokens-filter=', '') : '';
   const [filterString, setFilterString] = React.useState(initialFilterString);
   const [tokens, setTokens] = React.useState<TokenCategory[] | null>(getTokensByTheme(props, theme));
 
-  // useMemo
-  // React.useEffect(() => {
-  //   // setTokens(getTokensByTheme(props, theme));
-  //   setTokens(filterTokenList(filterString, props, theme));
-  // }, [theme]);
+  console.log('PROPS!!!!!!!!!! ---->', props);
 
+  // The rendered tokens should update every time the filterString, props, or theme changes
   React.useEffect(() => {
     setTokens(filterTokenList(filterString, props, theme));
     trackTokenFilterString(filterString);
@@ -150,6 +50,7 @@ export const TokensList: React.FC<TokensListProps> = (props) => {
   const handleInput = (e: React.FormEvent<HTMLInputElement>): void => {
     const filter = e.currentTarget.value;
     setFilterString(filter);
+    // Remove the search params from the URL if the filterString is removed
     if (filter === '') navigate(pathname, {replace: true});
     else navigate(`?tokens-filter=${filter}`, {replace: true});
   };
